aa204bb6a5cd129b4e4def2262545238d1e43c74
Yoonjin <elodie@kakao.com>
2020-09-25 12:47:46 +0900

    App.js 반환값이 많은 경우 괄호로 감싸서 처리

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Do it! 클론 코딩 영화 평점 웹서비스 따라하기
 
-React JS Fundamentals Couse 2020
+React JS Fundamentals Course 2020
--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div>hello</div>;
+  // App() 함수의 반환값이 많아지면 소괄호로 감싼다.
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
