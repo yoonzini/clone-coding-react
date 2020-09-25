b0286e62162ca7129991840ffe2ae41a1528e51b
Yoonjin <elodie@kakao.com>
2020-09-25 18:11:27 +0900

    03. 리액트 기초개념 알아보기 따라하기 중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+function Movie() {
+  return <h1>I like potato</h1>;
+}
+
 function App() {
   // App() 함수의 반환값이 많아지면 소괄호로 감싼다.
   return (
     <div>
       <h1>hello</h1>
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 ReactDOM.render(<App />,  document.getElementById('root'));
+// 리액트는 최종으로 단 한개의 컴포넌트를 그려야 한다.
+// 따라서 2개를 넣으면 에러가 발생한다.
+// app안에 potato를 넣어서 해결한다.
