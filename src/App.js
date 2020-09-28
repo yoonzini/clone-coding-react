13532f029001a49d25315bffd775f7a5b9e3d435
Yoonjin <elodie@kakao.com>
2020-09-28 18:40:52 +0900

    props 컴포넌트에서 활용하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Movie() {
-  return <h1>I like potato</h1>;
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
 }
 
 function App() {
@@ -9,18 +9,14 @@ function App() {
   return (
     <div>
       <h1>hello</h1>
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
+      <Food fav="ramen"/> 
+      <Food fav="soup"/> 
+      <Food fav="pizza"/> 
     </div>
-  );
+  ); 
+  // fav props 값 전달 'props에 있는 데이터는 문자열인 경우 제외하면 모두 {} 감싸야 한다.
+  // props에 있는 데이터를 객체로 변환하여 Food 컴포넌트(함수)에 전달한다.
+  // 전달받은 props를 props라는 인자로 받아 출력하였다.
 }
 
 export default App;
